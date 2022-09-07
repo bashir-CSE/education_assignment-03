7e764e324cd40bda1947919b56fb9b827588889c
bashir-CSE <babashir8811@gmail.com>
2022-09-07 22:20:15 +0600

    write somethings here

--- app.js
+++
@@ -1,6 +0,0 @@
-const btn = document.querySelectorAll('.navbar-toggler-icon');
-const nav_collaps = document.querySelector('.nav_collaps');
-
-function myFunction() {
-  nav_collaps.classList.add("show")
-}
