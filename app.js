56d0b9658ca5d61bdb2d3707d3f4078986f74707
bashir-CSE <babashir8811@gmail.com>
2022-09-07 17:16:34 +0600

    carousel responsive issue solve

---
+++ app.js
@@ -0,0 +1,6 @@
+const btn = document.querySelectorAll('.navbar-toggler-icon');
+const nav_collaps = document.querySelector('.nav_collaps');
+
+function myFunction() {
+  nav_collaps.classList.add("show")
+}
